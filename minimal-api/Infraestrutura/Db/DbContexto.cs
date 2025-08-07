80e332c32804849b63195a1ed978d4532d7acdf9
Glauco Diogo <glaucodiogoalves@gmail.com>
2025-08-07 02:26:46 -0300

    add migration admin

--- minimal-api/Infraestrutura/Db/DbContexto.cs
+++ minimal-api/Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,13 @@
+using MinimalApi.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
 namespace MinimalApi.infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
-    
+    public DbContexto(DbContextOptions<DbContexto> options) : base(options)
+    {
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
 }
--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,5 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using MinimalApi.Dtos;
+using MinimalApi.infraestrutura.Db;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("mysql"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
